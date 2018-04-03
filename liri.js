da638c37bf48be9cf440ea5625977c25ebeee51d
Corey Richard <ullrtheglorious@gmail.com>
2018-04-02 21:21:38 -0700

    changed to inquirer

--- liri.js
+++ liri.js
@@ -1,4 +1,5 @@
 require("dotenv").config();
+var inquirer = require("inquirer");
 var fs = require("fs");
 var request = require("request");
 var Twitter = require("twitter");
@@ -10,35 +11,57 @@ var omdbApi = (keys.omdb);
 var input = process.argv[2];
 var input2 = process.argv[3];
 
-switch (input) {
-// twitter input
-    case "my-tweets":
-        myTweets();
-    break;
-//  spoitify input
-    case "spotify-this-song":
-        if (input2) {
-            spotifySong(input2);
-        } else {
-            spotifySong('hello');
+bot()
+function bot() {
+    inquirer.prompt({
+        type: "list",
+        message: "Greetings, please make your selection.",
+        choices: ["My Tweets", "Movie This", "Spotify This", "Do what it says"],
+        name: "command"
+    }).then(function (data) {
+        switch (data.command) {
+        // twitter input
+            case "My Tweets":
+                myTweets();
+                setTimeout(function () {
+                    bot();
+                }, 3000);
+            break;
+        //  spoitify input
+            case "Spotify This":
+                inquirer.prompt({
+                    type: "input",
+                    message: "Search a song",
+                    name: "title"
+                }).then(function (data) {
+                    spotifySong(data.title);
+                    setTimeout(function () {
+                        bot();
+                    }, 3000);
+                })
+                break; 
+        // omdb input
+            case "Movie This":
+                inquirer.prompt({
+                    type: "input",
+                    message: "Search a movie",
+                    name: "title"
+                }).then(function (data) {
+                    movieThis(data.title);
+                    setTimeout(function () {
+                        bot();
+                    }, 3000);
+                })
+                break;
+        // random function
+            case "Do what it says":
+                randomThing();
+                setTimeout(function () {
+                    bot();
+                }, 3000);
+            break;
         }
-    break;
-// omdb input
-    case "movie-this":
-        if (input2) {
-            movie(input2)
-        } else {
-            movie("Mr. Nobody")
-        }
-    break;
-// random function
-    case "do-what-it-says":
-        randomThing();
-    break;
-// default liri function
-    default:
-        liriDefault();
-    break;
+    })
 }
 
 
@@ -67,7 +90,61 @@ function randomThing() {
     });
 }
 
-
+function movieThis(movie) {
+    var title = movie;
+    if (title === '') {
+        title = "Mr. Nobody"
+    }
+    var queryUrl = "http://www.omdbapi.com/?t=" + title + "&y=&plot=short&apikey=" + omdbApi.key;
+    request(queryUrl, function (error, response, body) {
+        if (!error && response.statusCode === 200) {
+            var body = JSON.parse(body);
+            console.log("Title: " + body.Title);
+            console.log("Year: " + body.Year);
+            console.log("IMDB Rating: " + body.imdbRating);
+            console.log("Rotten Tomatoes Score: " + body.Ratings[1].Value);
+            console.log("Country produced in: " + body.Country);
+            console.log("Language: " + body.Language);
+            console.log("Plot: " + body.Plot);
+            console.log("Actors: " + body.Actors);
+            fs.appendFile("log.txt", "Title: " + body.Title + "\r\n", (error) => { /* handle error */ });
+            fs.appendFile("log.txt", "Year: " + body.Year + "\r\n", (error) => { /* handle error */ });
+            fs.appendFile("log.txt", "IMDB Rating: " + body.imdbRating + "\r\n", (error) => { /* handle error */ });
+            fs.appendFile("log.txt", "Rotten Tomatoes Score: " + body.Ratings[1].Value + "\r\n", (error) => { /* handle error */ });
+            fs.appendFile("log.txt", "Country produced in: " + body.Country + "\r\n", (error) => { /* handle error */ });
+            fs.appendFile("log.txt", "Language: " + body.Language + "\r\n", (error) => { /* handle error */ });
+            fs.appendFile("log.txt", "Plot: " + body.Plot + "\r\n", (error) => { /* handle error */ });
+            fs.appendFile("log.txt", "Actors: " + body.Actors + "\r\n", (error) => { /* handle error */ });
+            console.log("----------------------------------------------------------------");
+            fs.appendFile("log.txt", "----------------------------------------------------------------" + "\r\n", (error) => { /* handle error */ });
+        }
+    });
+}
+function spotifySong(song) {
+    if (song === '') {
+        song = 'The Sign Ace Base';
+    }
+    spotify.search({ type: 'track', query: song }, function (err, data) {
+        if (err) {
+            return console.log('Error occurred: ' + err);
+        }
+        else {
+            var songInfo = data.tracks.items[0];
+            console.log("Artist: " + songInfo.artists[0].name)
+            console.log("Track: " + songInfo.name)
+            console.log("Album: " + songInfo.album.name)
+            console.log("Preview: " + songInfo.preview_url)
+            console.log("----------------------------------------------------------------");
+            fs.appendFile("log.txt", "Artist: " + songInfo.artists[0].name + "\r\n", (error) => { /* handle error */ })
+            fs.appendFile("log.txt", "Track: " + songInfo.name + "\r\n", (error) => { /* handle error */ })
+            fs.appendFile("log.txt", "Album: " + songInfo.album.name + "\r\n", (error) => { /* handle error */ })
+            fs.appendFile("log.txt", "Preview: " + songInfo.preview_url + "\r\n", (error) => { /* handle error */ })
+            setTimeout(function () {
+                fs.appendFile("log.txt", "----------------------------------------------------------------" + "\r\n", (error) => { /* handle error */ });
+            }, 500)
+        }
+    });
+}
 
 
 
